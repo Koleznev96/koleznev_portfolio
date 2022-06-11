5c7097ddfdbbee62dab96cd8d7c237f1a12a2422
Aleksey Koleznev <fc.fenomen@mail.ru>
2022-06-11 22:10:13 +0700

    update server

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(require('cors')());
 
-app.use('/', express.static(path.join(__dirname, '../client', 'build')));
+app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 const server = http.createServer(app);
